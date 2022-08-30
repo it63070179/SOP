524c771ec7a9b742240f5a3fd147a4a04a827608
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-08-30 07:34:39 +0700

    Add files via upload

--- FirstService/src/main/java/com/example/firstservice/GeneratePasswordService.java
+++ FirstService/src/main/java/com/example/firstservice/GeneratePasswordService.java
@@ -1,17 +1,28 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.firstservice;
 
+import java.util.Random;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.*;
 
 @RestController
 public class GeneratePasswordService {
-    @RequestMapping(value = "{name:[a-z]+}.generate", method = RequestMethod.GET)
-    public String generate(@PathVariable("name") String name){
+    public GeneratePasswordService() {
+    }
+
+    @RequestMapping(
+            value = {"{name:[a-z]+}.generate"},
+            method = {RequestMethod.GET}
+    )
+    public String generate(@PathVariable("name") String name) {
         Random num = new Random();
-        int num_random = num.nextInt();
-        return "Hi, " + name + "\n" + "Your new Password is " + num_random;
+        int num_random = num.nextInt(999999999);
+        return "Hi, " + name + "\nYour new Password is " + num_random;
     }
 }
